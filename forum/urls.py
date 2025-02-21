e0885c50cd60625a8f9d1e0829d5052327f41311
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-21 18:15:42 +0800

    Created index view for forum

--- forum/urls.py
+++ forum/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("threads", views.index, name="index"),
+    path("threads", views.IndexView.as_view(), name="index"),
     path("thread/<int:pk>", views.thread_detail, name="thread_detail"),
 ]
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+from .models import Post
+
+
+class IndexView(generic.ListView):
+    template_name = "forum/index.html"
+    context_object_name = "post_list"
+
+    def get_queryset(self):
+        return Post.objects.all()

ac29c93bc249489c7e5f5fcc4ba335cbcd14cf19
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-02-21 17:56:24 +0800

    Post model craeted

--- forum/models.py
+++ forum/models.py
@@ -4,3 +4,14 @@ from django.db import models
 class PostCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(PostCategory, on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-created_on"]

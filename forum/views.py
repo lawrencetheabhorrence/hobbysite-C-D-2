19986eaf795096ab7f83c329e8672a802a6f50f5
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-01 06:04:59 +0800

    Index now shows categories instead of posts, which leads to a page showing posts with that respective category

--- forum/views.py
+++ forum/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 
 # Create your views here.
-from .models import Post
+from .models import Post, PostCategory
 
 
 class IndexView(generic.ListView):
@@ -10,7 +10,12 @@ class IndexView(generic.ListView):
     context_object_name = "post_list"
 
     def get_queryset(self):
-        return Post.objects.all()
+        return PostCategory.objects.all()
+
+
+def PostByCategoryView(request, category):
+    posts = Post.objects.filter(category__name=category)
+    return render(request, "forum/post_list.html", {"post_list": posts})
 
 
 class PostDetailView(generic.DetailView):

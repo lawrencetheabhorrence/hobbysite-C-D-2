0cd538dfd8b41b795237d347439609e731015b3c
Goodymind <aabuke9@gmail.com>
2025-03-02 17:17:58 +0800

    Article

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class ArticleCategory(models.Model):
 
     def __str__(self):
         return f'{self.name}'
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField()
+    updated_on = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.title}, {self.category}, {self.entry}, {self.created_on}, {self.updated_on}'

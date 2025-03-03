62608fa66f53880f779927c46f8afca14775c7e0
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-03 21:30:26 +0800

    Added app name to urls file for namespacing urls in templates

--- forum/urls.py
+++ forum/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "forum"
 urlpatterns = [
     path("threads", views.IndexView.as_view(), name="index"),
     path("thread/<int:pk>", views.PostDetailView.as_view(), name="thread_detail"),

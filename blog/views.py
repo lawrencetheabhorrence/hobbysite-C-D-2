c3b5bb2cb8e3e4cc4365f6198702016ed8f868b4
Goodymind <aabuke9@gmail.com>
2025-03-04 18:02:20 +0800

    renamed to index and urls.py

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render
 from .models import Article, ArticleCategory
 
-def ArticleListView(request):
+def index_view(request):
     articles = Article.objects.all()
     context = {
         "articles": articles
     }
 
-    return render(request, "article_list.html", context)
+    return render(request, "blog/article_list.html", context)
 
-def ArticleDetailView(request, id):
+def detail_view(request, id):
     article = Article.objects.get(id=id)
     context = {
         "article" : article
     }
 
-    return render(request, "article_detail.html", context)
+    return render(request, "blog/article_detail.html", context)

ca5f709d5370629088c02f5eb626f3883f050415
ennybor11 <ennybor11@gmail.com>
2025-03-08 22:22:29 +0800

    Added unit tests

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Article, ArticleCategory
+
+class ArticleModelTest(TestCase):
+    def setUp(self):
+        self.category = ArticleCategory.objects.create(name="Tech", description="Tech articles")
+        self.article = Article.objects.create(
+            title="Sample Article",
+            category=self.category,
+            entry="This is a test entry."
+        )
+
+    def test_article_creation(self):
+        self.assertEqual(self.article.title, "Sample Article")
+        self.assertEqual(self.article.category.name, "Tech")

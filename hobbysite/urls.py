2ae63679ee35d2c1ac765840e412c86349917dc6
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-09 17:56:47 +0800

    Updated settings.py and urls.py to account for all available apps

--- hobbysite/urls.py
+++ hobbysite/urls.py
@@ -20,5 +20,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('wiki/', include('wiki.urls')),
+    path("blog/", include("blog.urls")),
+    path("forum/", include("forum.urls")),
+    path("merchstore/", include("merchstore.urls")),
+    path("wiki/", include("wiki.urls")),    
 ]

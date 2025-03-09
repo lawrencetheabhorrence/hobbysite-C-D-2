21975baea9b49e8949799ef96c6b78c712c5b8e3
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-09 18:57:03 +0800

    Migrated changes

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -12,11 +12,11 @@ class ProductTypeAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     model = Product
 
-    list_display = ('name','product_type__name','price',)
-    list_filter = ('recipe__name',)
+    list_display = ('name','product_type','price',)
+    list_filter = ('name',)
     search_fields = ['product_type__name']
     list_display_links = None
-    list_editable = ('name','product_type__name','price',)
+    list_editable = ('name','product_type','price',)
 
 admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Product, ProductAdmin)

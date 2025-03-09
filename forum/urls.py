2c20d550b22e84b6609c2cfe835b2ea0f7dd2a5e
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-09 19:22:19 +0800

    Fully removed apps for the sake of this branch

--- forum/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = "forum"
-urlpatterns = [
-    path("threads", views.index_view, name="index"),
-    path("thread/<int:pk>", views.detail_view, name="thread_detail"),
-]
--- wiki/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('articles/', views.article_list, name='article-list'),
-    path('article/<int:pk>/', views.article_detail, name='article-detail'),
-]

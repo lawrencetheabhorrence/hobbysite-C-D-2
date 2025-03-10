c6fce50947de3a67a8261954945c868b9f463a70
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-10 08:24:28 +0800

    Created admin panel specifications of merchstore in admin.py

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import ProductType, Product
 
-# Register your models here.
+class ProductTypeAdmin(admin.ModelAdmin):
+    model = ProductType
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    list_display_links = None
+    list_editable = ('name',)
+
+class ProductAdmin(admin.ModelAdmin):
+    model = Product
+
+    list_display = ('name','product_type__name','price',)
+    list_filter = ('recipe__name',)
+    search_fields = ['product_type__name']
+    list_display_links = None
+    list_editable = ('name','product_type__name','price',)
+
+admin.site.register(ProductType, ProductTypeAdmin)
+admin.site.register(Product, ProductAdmin)

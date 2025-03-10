95d9a5285ba0439cc214a33ac1ab84e9aa1d195b
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-10 08:16:12 +0800

    Corrected imported merchstoreEntry to merchstoreItem from .views in urls.py

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import merchstoreList, merchstoreEntry
+from .views import merchstoreList, merchstoreItem
 
 urlpatterns = [
     path('merchstore/items/', merchstoreList, name="merchstoreList"),
     path('merchstore/item/<int:num>/', merchstoreItem, name="merchstoreItem"),
     ]
+
+#app_name = "merchstore"

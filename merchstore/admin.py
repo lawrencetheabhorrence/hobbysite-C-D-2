a73769553143ae7d33b13df4ee2147cca413db80
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-03-10 08:32:42 +0800

    Primary keys and revisions in models.py, edited admin.py to accommodate

--- merchstore/admin.py
+++ merchstore/admin.py
@@ -4,19 +4,19 @@ from .models import ProductType, Product
 class ProductTypeAdmin(admin.ModelAdmin):
     model = ProductType
 
-    list_display = ('name',)
+    list_display = ('name','productTypeID',)
     list_filter = ('name',)
-    list_display_links = None
+    list_display_links = ('productTypeID',)
     list_editable = ('name',)
 
 class ProductAdmin(admin.ModelAdmin):
     model = Product
 
-    list_display = ('name','product_type__name','price',)
-    list_filter = ('recipe__name',)
-    search_fields = ['product_type__name']
-    list_display_links = None
-    list_editable = ('name','product_type__name','price',)
+    list_display = ('name','product_type','price','productID',)
+    list_filter = ('product_type',)
+    search_fields = ['product_type']
+    list_display_links = ('productID',)
+    list_editable = ('name','product_type','price',)
 
 admin.site.register(ProductType, ProductTypeAdmin)
 admin.site.register(Product, ProductAdmin)
--- merchstore/models.py
+++ merchstore/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.urls import reverse
 
 class ProductType(models.Model):
+    productTypeID = models.IntegerField(primary_key=True,default=0,editable=True)
     name = models.CharField(max_length=255)
+    description = models.TextField(default="Some variety of items sold in this merch store.")
+
+    def get_absolute_url(self):
+        return reverse("merchstoreSublist",kwargs={"product_type":str(self.name)})
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -13,15 +18,16 @@ class ProductType(models.Model):
         verbose_name = 'ProductType'
         verbose_name_plural = 'ProductTypes'
 
-    description = models.TextField(default="Some variety of items sold in this merch store.")
 
 class Product(models.Model):
+    productID = models.IntegerField(primary_key=True,default=0,editable=True)
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(ProductType, null=True, on_delete = models.SET_NULL, related_name = 'ProductType')
     price = models.DecimalField(max_digits=7,decimal_places=2)
-
+    description = models.TextField(default="A buyable item of this merch store.")
+    
     def get_absolute_url(self):
-        return reverse("merchstoreItem",kwargs={"num":int(self.id)})
+        return reverse("merchstoreItem",kwargs={"num":int(self.productID)})
 
     def __str__(self):
         return '{}'.format(self.name)
@@ -31,5 +37,3 @@ class Product(models.Model):
         unique_together = ['name']
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
-
-    description = models.TextField(default="A buyable item of this merch store.")

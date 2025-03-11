afa706a05fd4103f911bcc07d6ceaeec40adf219
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-11 15:01:10 +0800

    Customized admin for Post

--- forum/admin.py
+++ forum/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 
 from .models import PostCategory, Post
 
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ["title", "category", "entry"]
+    list_display = ["title", "category"]
+    list_filter = ["category"]
+
+
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(PostCategory)

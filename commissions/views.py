fe84bf93030b8a5386f9b9d66b5a99ede54991e5
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-03-13 18:44:11 +0800

    Commissions_views_edit+templates/commissions
    edited views to use get_list_or_404 and get_object_or_404
    made templates/commissions

--- commissions/views.py
+++ commissions/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Commission, Comment
 
 def commission_list(request):
-    commissions = Commission.objects.all().order_by('-created_on')
+    commissions = get_list_or_404(Commission)
 
     return render(request, 'commissions_list.html', {'commissions': commissions})
 
-
 def commission_detail(request, commission_id):
-    commission = Commission.objects.filter(id=commission_id)
-    comments = Comment.objects.filter(commission=commission).order_by('-created_on')
+    commission = get_object_or_404(Commission, id=commission_id)
+    
+    comments = Comment.objects.filter(commission=commission)
 
     return render(request, 'commissions_detail.html', {'commission': commission, 'comments': comments})
 

88b5e3fc85f9644b3707f3f11c56cef3bb99867d
ennybor11 <ennybor11@gmail.com>
2025-03-14 15:27:33 +0800

    Updated admin with list view

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import ArticleCategory, Article
-# Register your models here.
-admin.site.register(ArticleCategory)
-admin.site.register(Article)
+
+@admin.register(ArticleCategory)
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")  
+    search_fields = ("name",)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Article._meta.fields]  
+    list_filter = ("category",)  
+    search_fields = ("title",)  

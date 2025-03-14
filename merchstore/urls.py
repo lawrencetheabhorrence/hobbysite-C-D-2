32527abdd2b35715a8cc5c137700f75ff38f2001
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-14 21:34:43 +0800

    Views changed to fit simplified templates, template fixes

--- merchstore/urls.py
+++ merchstore/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import merchstoreList, merchstoreItem
 
+app_name = "merchstore"
 urlpatterns = [
     path("items/", merchstoreList, name="merchstore_list"),
     path("item/<int:itemID>/", merchstoreItem, name="merchstore_item"),

699a1c831aa0d36ac4bada3d8dc15d8f99738acb
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-03-14 20:31:11 +0800

    Created on field removed from admin editable fields

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,8 +3,10 @@ from .models import ArticleCategory, Article
 
 
 class ArticleAdmin(admin.ModelAdmin):
-    fields = ["title", "category", "entry", "created_on"]  # Specify fields manually
-    list_display = [field.name for field in Article._meta.fields]  # Keep dynamic field retrieval
+    fields = ["title", "category", "entry"]  # Specify fields manually
+    list_display = [
+        field.name for field in Article._meta.fields
+    ]  # Keep dynamic field retrieval
     list_filter = ["category"]
     search_fields = ["title"]
 

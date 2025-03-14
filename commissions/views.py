7ad220abee6c01d85cc080703e021d84e6e732ec
Goodymind <aabuke9@gmail.com>
2025-03-14 13:59:32 +0800

    commissions url and file path fixes

--- commissions/views.py
+++ commissions/views.py
@@ -4,13 +4,13 @@ from .models import Commission, Comment
 def commission_list(request):
     commissions = get_list_or_404(Commission)
 
-    return render(request, 'commissions_list.html', {'commissions': commissions})
+    return render(request, 'commissions/commissions_list.html', {'commissions': commissions})
 
 def commission_detail(request, commission_id):
     commission = get_object_or_404(Commission, id=commission_id)
     
     comments = Comment.objects.filter(commission=commission)
 
-    return render(request, 'commissions_detail.html', {'commission': commission, 'comments': comments})
+    return render(request, 'commissions/commissions_detail.html', {'commission': commission, 'comments': comments})
 
 # Create your views here.

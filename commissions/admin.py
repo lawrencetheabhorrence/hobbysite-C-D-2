0f21289bbe5d185edb24d73022497bd54d899cb4
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-03-14 12:43:35 +0800

    Commissions_admin
    list view that shows all fields of the model then filter by category

--- commissions/admin.py
+++ commissions/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 from .models import Commission, Comment
 
-admin.site.register(Commission)
-admin.site.register(Comment)
 
-# Register your models here.
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+    list_display = ("title", "description", "people_required", "created_on", "updated_on")
+    list_filter = ("created_on", "people_required")
+    list_display_links = ("title",)
+    list_editable = ("description", "people_required")
+    search_fields = ("title",)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+    list_display = ("commission", "entry", "created_on", "updated_on")
+    list_filter = ("created_on", "commission")
+    search_fields = ("commission__title", "entry")
+    list_display_links = ("commission",)
+    list_editable = ("entry",)
+
+
+admin.site.register(Commission, CommissionAdmin)
+admin.site.register(Comment, CommentAdmin)

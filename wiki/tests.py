ed7b7625fb557080b8acdacad8e92daa4580a319
ennybor11 <ennybor11@gmail.com>
2025-03-14 15:35:14 +0800

    Resolved a merge conflict in tests.py

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,11 +1,6 @@
 from django.test import TestCase
 from .models import Article, ArticleCategory
 
-<<<<<<< HEAD
-from .models import Article, ArticleCategory
-
-=======
->>>>>>> 587c4d94a55b9b5ac3273af78ed70296eeece7e7
 class ArticleModelTest(TestCase):
     def setUp(self):
         self.category = ArticleCategory.objects.create(name="Tech", description="Tech articles")
@@ -17,8 +12,4 @@ class ArticleModelTest(TestCase):
 
     def test_article_creation(self):
         self.assertEqual(self.article.title, "Sample Article")
-<<<<<<< HEAD
         self.assertEqual(self.article.category.name, "Tech")
-=======
-        self.assertEqual(self.article.category.name, "Tech")
->>>>>>> 587c4d94a55b9b5ac3273af78ed70296eeece7e7

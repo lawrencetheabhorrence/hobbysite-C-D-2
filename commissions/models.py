b675cd083809bbf7a0518a46a35efd0c1e43e7d2
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-06 22:00:19 +0800

    Amended commission model

--- commissions/models.py
+++ commissions/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 class Commission(models.Model):
+    class CommissionStatusOptions(models.TextChoices):
+        OPEN = "Open"
+        FULL = "Full"
+        COMPLETED = "Completed"
+        DISCONTINUED = "Discontinued"
+
     title = models.CharField(max_length=255)
     description = models.TextField()
-    people_required = models.IntegerField()
+    status = models.CharField(
+        default=CommissionStatusOptions.OPEN, choices=CommissionStatusOptions
+    )
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 

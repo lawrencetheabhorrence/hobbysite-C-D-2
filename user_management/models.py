a106d9b2cb15872c1d699f579c04ca88dda18417
Goodymind <aabuke9@gmail.com>
2025-04-11 22:40:58 +0800

    profile model

--- user_management/models.py
+++ user_management/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Us):
+    '''
+        Extension of user model\n
+        has fields:\n
+            name - max length of 63 characters
+            email_address - email field
+    '''
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=63)
+    email_address = models.EmailField()
+
+    def __str__(self):
+        return self.name

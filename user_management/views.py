1f07353c28559c0c60160c07f47f36ff395abd4a
Goodymind <aabuke9@gmail.com>
2025-04-11 22:58:32 +0800

    update view

--- user_management/views.py
+++ user_management/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+def update(request, id):
+    
+    profile = Profile.objects.get(id=id)
+    
+    if request.method == 'POST':
+        
+        profile.name = request.POST.get('new_name')
+        profile.email_address = request.POST.get('new_email_address')
+        profile.save()
+    
+    context = {'profile': profile}
+    
+    return render(request, 'update.html', context)

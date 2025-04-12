5de5343ccf4e9457a03c58e7df123746c95452ea
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-04-12 18:56:35 +0800

    Applied suggestions on choices

--- merchstore/models.py
+++ merchstore/models.py
@@ -18,20 +18,8 @@ class ProductType(models.Model):
     def __str__(self):
         return self.name
 
-    #May get deleted...unless there's still time
-    '''
-    def get_absolute_url(self):
-        return reverse("merchstore_variety", kwargs={"product_type": self.name})
-    '''
-
 
 class Product(models.Model):
-    STATUS_CHOICES = {
-        "Available": "Available",
-        "On Sale": "On Sale",
-        "Out of Stock": "Out of Stock"
-    }
-
     name = models.CharField(max_length=255)
     product_type = models.ForeignKey(
         ProductType, null=True, on_delete=models.SET_NULL, related_name="ProductType"
@@ -41,10 +29,24 @@ class Product(models.Model):
         Profile, null=False, on_delete=models.CASCADE, related_name="Owner"
         )
     '''
-    description = models.TextField(default="A buyable item of this merch store.")
+    description = models.TextField(
+        default="A buyable item of this merch store."
+        )
     price = models.DecimalField(max_digits=24, decimal_places=2)
     stock = models.PositiveIntegerField(default=0)
-    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default="Out of Stock")
+
+    
+    class ProductStatusChoices(models.TextChoices):
+        AVAILABLE = "Available"
+        ON_SALE = "On Sale"
+        OUT_OF_STOCK = "Out of Stock"
+
+
+    status = models.CharField(
+        max_length=12,
+        choices=ProductStatusChoices,
+        default=ProductStatusChoices.OUT_OF_STOCK
+        )
 
 
     class Meta:
@@ -61,13 +63,6 @@ class Product(models.Model):
 
 
 class Transaction(models.Model):
-    STATUS_CHOICES = {
-        "On Cart": "On Cart",
-        "To Pay": "To Pay",
-        "To Ship": "To Ship",
-        "To Receive": "To Receive",
-        "Delivered": "Delivered",
-    }
     '''
     buyer = models.ForeignKey(
         Profile, null=True, on_delete=models.SET_NULL, related_name="Buyer"
@@ -77,7 +72,21 @@ class Transaction(models.Model):
         Product, null=True, on_delete=models.SET_NULL, related_name="Product"
         )
     amount = models.PositiveIntegerField()
-    status = models.CharField(max_length=12, choices=STATUS_CHOICES)
+
+
+    class TransactionStatusChoices(models.TextChoices):
+        ON_CART = "On Cart"
+        TO_PAY = "To Pay"
+        TO_SHIP = "To Ship"
+        TO_RECEIVE = "To Receive"
+        DELIVERED = "Delivered"
+
+    
+    status = models.CharField(
+        max_length=12,
+        choices=TransactionStatusChoices,
+        default=TransactionStatusChoices.ON_CART
+        )
     created_on = models.DateTimeField(auto_now_add=True)
 
 

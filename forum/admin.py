585bfe4820b0817da925f8694f0b65b760be8676
ennybor11 <ennybor11@gmail.com>
2025-04-12 22:55:28 +0800

    Edited Article and ArticleCategory models, added Comment model

--- forum/admin.py
+++ forum/admin.py
@@ -13,7 +13,6 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "category"]
     list_filter = ["category"]
 
-
-# Register your models here.
 admin.site.register(Post, PostAdmin)
-admin.site.register(PostCategoryAdmin, PostCategory)
+# admin.site.register(PostCategoryAdmin, PostCategory)
+admin.site.register(PostCategory, PostCategoryAdmin) # attempt to fix TypeError: 'MediaDefiningClass' object is not iterable

b46a85d2a3286e7c63c2025e505482c8773abc48
Goodymind <aabuke9@gmail.com>
2025-04-13 22:36:56 +0800

    comments retyped

--- user_management/models.py
+++ user_management/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     '''
-        Extension of user model\n
-        has fields:\n
-            name - max length of 63 characters
-            email_address - email field
+        Extension of user model
+        has fields:
+        name - max length of 63 characters
+        email_address - email field
     '''
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=63)

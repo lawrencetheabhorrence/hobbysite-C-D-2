9a10bc92899af93146ab500a498d0f4f83eba854
Goodymind <aabuke9@gmail.com>
2025-04-13 23:18:02 +0800

    blog_models as stated by the docs

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user_management.models import Profile
 
 
 class ArticleCategory(models.Model):
@@ -18,8 +19,10 @@ class ArticleCategory(models.Model):
 class Article(models.Model):
 
     title = models.CharField(max_length=255)
+    author = models.OneToOneField(Profile, on_delete=models.SET_NULL)
     category = models.ForeignKey(ArticleCategory, null=True, on_delete=models.SET_NULL)
     entry = models.TextField()
+    header_image = models.ImageField(null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -30,3 +33,19 @@ class Article(models.Model):
     class Meta:
 
         ordering = ["-created_on"]
+
+
+class Comment(models.Model):
+
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.author.name}, {self.created_on}, {self.article.title}"
+
+    class Meta:
+
+        ordering = ["created_on"]

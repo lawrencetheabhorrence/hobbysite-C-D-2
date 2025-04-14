13f0107d602841fc0a683c1cf926ef371bdd0d18
ennybor11 <ennybor11@gmail.com>
2025-04-14 10:06:18 +0800

    removed commented line

--- forum/admin.py
+++ forum/admin.py
@@ -14,5 +14,4 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ["category"]
 
 admin.site.register(Post, PostAdmin)
-# admin.site.register(PostCategoryAdmin, PostCategory)
 admin.site.register(PostCategory, PostCategoryAdmin) # attempt to fix TypeError: 'MediaDefiningClass' object is not iterable

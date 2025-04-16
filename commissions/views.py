61502df469952520f1d2afc9ac210e76073c1721
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-16 18:58:22 +0800

    import fix in views

--- commissions/views.py
+++ commissions/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
-from .models import Commission
+from .models import Commission, Job
 
 
 class CommissionListView(ListView):

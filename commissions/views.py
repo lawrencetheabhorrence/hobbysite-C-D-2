bafa6e2b868e6d3e70c8dc05e0ee73627c1812ea
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-16 19:52:29 +0800

    added functionality for buttons in template

--- commissions/views.py
+++ commissions/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic import DetailView
 from django.views.generic.list import ListView
-from .models import Commission, Job
+from .models import Commission, Job, JobApplication
 
 
 class CommissionListView(ListView):
@@ -24,3 +25,18 @@ class JobView(DetailView):
     model = Job
     template_name = "commissions/job_detail.html"
     context_object_name = "job"
+
+    def post(self, request, *args, **kwargs):
+        if "accept" in request.POST:
+            app_id = request.POST.get("accept")
+            application = get_object_or_404(JobApplication, id=app_id)
+            application.accept_job()
+
+        elif "reject" in request.POST:
+            app_id = request.POST.get("reject")
+            application = get_object_or_404(JobApplication, id=app_id)
+            application.reject_job()
+
+        else:
+            # redirect to self if somehow neither button was detected
+            return HttpResponseRedirect(self.request.path_info)

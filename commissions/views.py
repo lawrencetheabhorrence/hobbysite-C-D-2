c2cc1584130d2438016e423563d82976770b6302
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-16 11:01:31 +0800

    fixed import

--- commissions/views.py
+++ commissions/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
-from .models import Commission, Comment
+from .models import Commission
 
 
 class CommissionListView(ListView):

5c495bc81f86c5823bc2abbd1d9fc66e74f68a7d
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-04-17 12:24:08 +0800

    Cleanup

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import ProductType, Product
+from .models import ProductType, Product #, Transaction
 
 
 def merchstoreList(request):
@@ -9,7 +9,7 @@ def merchstoreList(request):
         {"product_list": Product.objects.all()},
     )
 
-
+'''
 def merchstoreVariety(request, product_type=""):
     # This function is deprecated
     # Shows products of a certain type
@@ -20,7 +20,7 @@ def merchstoreVariety(request, product_type=""):
     context = {"product_kind": chosen_product_type, "items": available_items}
 
     return render(request, "merchstore/merchstore_variety.html", context)
-
+'''
 
 def merchstoreItem(request, itemID):
     product = get_object_or_404(Product, pk=itemID)

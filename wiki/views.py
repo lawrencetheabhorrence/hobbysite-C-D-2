b53e88478d98f8449262be60e19a0d92688c2ef5
ennybor11 <ennybor11@gmail.com>
2025-04-17 15:18:41 +0800

    Removed get_context logic, will move that to template.

--- wiki/views.py
+++ wiki/views.py
@@ -1,6 +1,5 @@
 from django.views.generic.list import ListView
-from .models import Article
-from collections import defaultdict 
+from .models import Article 
 
 class ArticleListView(ListView):
     model = Article
@@ -10,21 +9,3 @@ class ArticleListView(ListView):
     def get_queryset(self):
         return Article.objects.select_related("category", "author")
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user_articles = []
-        categorized_articles = defaultdict(list)
-        articles = self.get_queryset()
-
-        if self.request.user.is_authenticated and hasattr(self.request.user, "profile"):
-            user_profile = self.request.user.profile
-            user_articles = articles.filter(author = user_profile)
-            articles = articles.exclude(author = user_profile)
-
-        for article in articles: 
-            if article.category:
-                categorized_articles[article.category.name].append(article)
-
-        context["user_articles"] = user_articles
-        context["categorized_articles"] = dict(categorized_articles)
-        return context

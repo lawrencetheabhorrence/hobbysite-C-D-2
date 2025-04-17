8219c43711ba8c1677e9c8129e9d3c731bf6d4bb
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-04-17 13:28:48 +0800

    List view can now determine whether logged-in user has products

--- merchstore/templatetags/merchstore_extra_tags.py
+++ merchstore/templatetags/merchstore_extra_tags.py
@@ -7,3 +7,7 @@ register = template.Library()
 @register.filter
 def products(product_type):
     return Product.objects.filter(product_type=product_type)
+
+@register.filter
+def count_owner_products(owner):
+    return len(Product.objects.filter(owner=owner))

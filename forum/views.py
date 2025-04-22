f4f059e7456471495fcfabf98a47445093e18e74
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-04-22 11:58:12 +0800

    Forum views - thread_list & thread_detail
    Updated thread list and thread detail
    included the templates

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,44 @@
-from django.shortcuts import render, get_list_or_404, get_object_or_404
-from .models import Post
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Thread, ThreadCategory
+from .forms import CommentForm
 
+def thread_list(request):
+    categories = ThreadCategory.objects.all()
+    user_threads = []
+    
+    if request.user.is_authenticated:
+        try:
+            user_threads = request.user.profile.threads.all()
+        except:
+            pass
+    
+    info = {
+        'categories': categories,
+        'user_threads': user_threads,
+    }
+    return render(request, 'forum/index.html', info)
 
-def index_view(request):
-    return render(request, "forum/index.html", {"post_list": get_list_or_404(Post)})
-
-
-def detail_view(request, pk):
-    return render(
-        request, "forum/post_detail.html", {"post": get_object_or_404(Post, pk=pk)}
-    )
+def thread_detail(request, pk):
+    thread = get_object_or_404(Thread, pk=pk)
+    related_threads = Thread.objects.filter(category=thread.category).exclude(pk=thread.pk)[:2]
+    comments = thread.comment_set.all().order_by('created_on')
+    
+    if request.method == 'POST' and request.user.is_authenticated:
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user.profile
+            comment.thread = thread
+            comment.save()
+            return redirect('forum:thread_detail', pk=thread.pk)
+    else:
+        form = CommentForm()
+    
+    info = {
+        'thread': thread,
+        'related_threads': related_threads,
+        'comments': comments,
+        'form': form,
+        'is_owner': request.user.is_authenticated and thread.author and thread.author.user == request.user,
+    }
+    return render(request, 'forum/detail.html', info)

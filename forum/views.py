3cb75643f95545dfeecf570796daa4435376b75d
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-04-24 12:11:53 +0800

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -1,45 +1,56 @@
+from django.views import View
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Thread, ThreadCategory
 from .forms import CommentForm
 
 
-class ThreadList:
-    def __call__(self, request):
+class ThreadList(View):
+    def get(self, request):
         categories = ThreadCategory.objects.all()
         user_threads = []
-        
+
         if request.user.is_authenticated:
             try:
                 user_threads = request.user.profile.threads.all()
-            except:
-                pass
-        
+            except AttributeError:
+                user_threads = []
+
         return render(request, 'forum/index.html', {
             'categories': categories,
             'user_threads': user_threads,
         })
 
 
-class ThreadDetail:
-    def __call__(self, request, pk):
+class ThreadDetail(View):
+    def get(self, request, pk):
         thread = get_object_or_404(Thread, pk=pk)
         related_threads = Thread.objects.filter(category=thread.category).exclude(pk=thread.pk)
-        
-        if request.method == 'POST' and request.user.is_authenticated:
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                comment.author = request.user.profile
-                comment.thread = thread
-                comment.save()
-                return redirect('forum:thread_detail', pk=thread.pk)
-        else:
-            form = CommentForm()
-        
+        form = CommentForm()
+
         return render(request, 'forum/detail.html', {
             'thread': thread,
             'related_threads': related_threads,
-            'comments': thread.comment_set.all(),  
+            'comments': thread.comment_set.all(),
+            'form': form,
+            'is_owner': request.user.is_authenticated and thread.author and thread.author.user == request.user,
+        })
+
+    def post(self, request, pk):
+        thread = get_object_or_404(Thread, pk=pk)
+        related_threads = Thread.objects.filter(category=thread.category).exclude(pk=thread.pk)
+        form = CommentForm(request.POST)
+
+        if request.user.is_authenticated and form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user.profile
+            comment.thread = thread
+            comment.save()
+            return redirect('forum:thread_detail', pk=thread.pk)
+
+        return render(request, 'forum/detail.html', {
+            'thread': thread,
+            'related_threads': related_threads,
+            'comments': thread.comment_set.all(),
             'form': form,
             'is_owner': request.user.is_authenticated and thread.author and thread.author.user == request.user,
         })

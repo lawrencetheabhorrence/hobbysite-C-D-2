a878bc0a33edefdf9a725bea31a1aefae8297d1d
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-04-25 17:17:44 +0800

    Update urls.py

--- forum/urls.py
+++ forum/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import ThreadListView, ThreadDetailView, ThreadCreate, ThreadUpdate
+from .views import ThreadListView, ThreadDetailView, ThreadCreateView, ThreadUpdateView
 
 
 app_name = "forum"
 urlpatterns = [
     path("threads/", ThreadListView.as_view(), name="index"),
     path("thread/<int:pk>/", ThreadDetailView.as_view(), name="detail"),
-    path("thread/add/", ThreadCreate.as_view(), name="thread_create"),
-    path("thread/<int:pk>/edit/", ThreadUpdate.as_view(), name="thread_update"),
+    path("thread/add/", ThreadCreateView.as_view(), name="thread_create"),
+    path("thread/<int:pk>/edit/", ThreadUpdateView.as_view(), name="thread_update"),
 ]

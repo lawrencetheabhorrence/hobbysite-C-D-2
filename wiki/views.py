b12b9dff46a4a80898785f0e25afb8a4979fd24c
ennybor11 <ennybor11@gmail.com>
2025-04-25 11:24:22 +0800

    related articles, comment form, edit link logic

--- wiki/views.py
+++ wiki/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from .models import Article
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from .models import Article, Comment, Image
+from .forms import CommentForm
+# from django.contrib.auth.decorators import login_required
 
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "wiki/article_detail.html"
+    context_object_name = "article"
 
-def article_list(request):
-    articles = get_list_or_404(Article)  # Raises 404 if no articles exist
-    return render(request, "wiki/article_list.html", {"articles": articles})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article = self.get_object()
+        
+        context["related_articles"] = article.category.articles.exclude(pk=article.pk)[:2]
 
+        context["comments"] = Comment.objects.filter(article=article).order_by('-created_on')
+        
+        if self.request.user.is_authenticated:
+            context["comment_form"] = CommentForm()
 
-def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)  # Raises 404 if article does not exist
-    return render(request, "wiki/article_detail.html", {"article": article})
+        context["images"] = Image.objects.filter(article=article)
+        
+        if article.author == self.request.user:
+            context["edit_link"] = True
+
+        return context
+
+    def post(self, request, *args, **kwargs):
+        article = self.get_object()
+        
+        if request.user.is_authenticated:
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.article = article
+                comment.user = request.user
+                comment.save()
+                return redirect('article-detail', pk=article.pk)  
+        return HttpResponse("You must be logged in to comment", status=403)

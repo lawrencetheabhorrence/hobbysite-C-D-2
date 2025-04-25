03be41244a7ba108d19e18e62b6fd35a2f139369
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-04-25 17:04:18 +0800

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -6,8 +6,15 @@ class ThreadListView(ListView):
     model = ThreadCategory
     template_name = "forum/index.html"
     context_object_name = "categories"
+    
 
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = "forum/detail.html"
     context_object_name = "thread"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['threads'] = Thread.objects.all()
+        
+        return context

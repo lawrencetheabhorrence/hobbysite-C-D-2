c0c30fc4077758e425f5d663075e2ce3ae2451d2
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-04-25 17:30:51 +0800

    ran black

--- forum/models.py
+++ forum/models.py
@@ -16,8 +16,12 @@ class ThreadCategory(models.Model):
 
 class Thread(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL, related_name="threads")
-    category = models.ForeignKey(ThreadCategory, null=True, on_delete=models.SET_NULL, related_name="threads")
+    author = models.ForeignKey(
+        Profile, null=True, on_delete=models.SET_NULL, related_name="threads"
+    )
+    category = models.ForeignKey(
+        ThreadCategory, null=True, on_delete=models.SET_NULL, related_name="threads"
+    )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
@@ -37,7 +41,7 @@ class Comment(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordering = ["created_on"]  
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment by {self.author.display_name} on {self.thread.title}"

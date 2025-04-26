2f5b0e09679395817c29edc36eca9a3ef172fd88
Goodymind <aabuke9@gmail.com>
2025-04-26 18:30:59 +0800

    removed function based views

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,3 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from .models import Article
 
 
-def index_view(request):
-    articles = get_list_or_404(Article)
-    context = {"articles": articles}
-
-    return render(request, "blog/index.html", context)
-
-
-def detail_view(request, id):
-    article = get_object_or_404(Article, id=id)
-    context = {"article": article}
-
-    return render(request, "blog/article_detail.html", context)

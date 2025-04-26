9b7a8e60f70bad13009d10ec438a84ca055d5afa
Goodymind <aabuke9@gmail.com>
2025-04-26 20:55:58 +0800

    black reformatting

--- blog/models.py
+++ blog/models.py
@@ -22,7 +22,7 @@ class Article(models.Model):
     author = models.OneToOneField(Profile, on_delete=models.SET_NULL)
     category = models.ForeignKey(ArticleCategory, null=True, on_delete=models.SET_NULL)
     entry = models.TextField()
-    header_image = models.ImageField(null=True, upload_to='uploads/blog/%Y/%m/%d/')
+    header_image = models.ImageField(null=True, upload_to="uploads/blog/%Y/%m/%d/")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from .models import Article
 
+
 class ArticleListView(ListView):
     model = Article
     template_name = "blog/blog_list.html"

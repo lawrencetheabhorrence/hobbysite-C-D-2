07fed1fc6feeb330c294101b1aec869140bb3d27
Goodymind <aabuke9@gmail.com>
2025-04-26 21:20:05 +0800

    ArticleDetailView class implemented

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Article
 
 
 class ArticleListView(ListView):
     model = Article
-    template_name = "blog/blog_list.html"
+    template_name = "blog/article_list.html"
+    context_object_name = "blogs"
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "blog/article_detail.html"
     context_object_name = "blogs"

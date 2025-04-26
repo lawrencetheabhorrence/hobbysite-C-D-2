a0e222735339aed7cb7ac9464d07a5d0b588600c
Mishka0123 <mishka.sy@student.ateneo.edu>
2025-04-26 10:08:51 +0800

    Forums - thread_create & thread_update
    added create and update to views and added respective templates

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import ThreadCategory, Thread
+from django.urls import reverse
+from .forms import ThreadForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+from user_management.models import Profile
 
 
 class ThreadListView(ListView):
@@ -9,12 +14,54 @@ class ThreadListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["categories"] = ThreadCategory.objects.all()
-
+        context['categories'] = ThreadCategory.objects.all()
+        
         return context
-
+    
 
 class ThreadDetailView(DetailView):
     model = Thread
     template_name = "forum/detail.html"
     context_object_name = "thread"
+
+
+class ThreadCreateView(LoginRequiredMixin, CreateView):
+    model = Thread
+    template_name = "forum/thread_create.html"
+    context_object_name = "thread"
+    form_class = ThreadForm
+
+    def form_valid(self, form):
+        form.instance.author = Profile.objects.get(user=self.request.user)  
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ThreadCategory.objects.all()  
+        return context
+
+    def get_success_url(self):
+        return reverse('thread_detail', kwargs={'pk': self.object.pk})  
+
+
+class ThreadUpdateView(LoginRequiredMixin, UpdateView):
+    model = Thread
+    template_name = "forum/thread_update.html"
+    context_object_name = "thread"
+    form_class = ThreadForm
+
+    def form_valid(self, form):
+        form.instance.updated_on = timezone.now()
+        return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = ThreadCategory.objects.all()  
+        return context
+
+    def get_success_url(self):
+        return reverse('thread_detail', kwargs={'pk': self.object.pk})
+
+
+
+   

820dc6162d4845ae70d0a08f7a5a27fbd331a6da
ennybor11 <ennybor11@gmail.com>
2025-04-26 12:36:25 +0800

    Added url

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+# from . import views
+from .views import ArticleCreateView
 
 app_name = "wiki"
 urlpatterns = [
-    path("articles/", views.article_list, name="article-list"),
-    path("article/<int:pk>/", views.article_detail, name="article-detail"),
+    path('article/add/', ArticleCreateView.as_view(), name='article-create')
 ]

fb309184d62d9655085bfe565cac62c99a177c2d
ennybor11 <ennybor11@gmail.com>
2025-04-26 12:59:09 +0800

    Added url

--- wiki/urls.py
+++ wiki/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 app_name = "wiki"
 urlpatterns = [
-    path("articles/", views.article_list, name="article-list"),
-    path("article/<int:pk>/", views.article_detail, name="article-detail"),
+    path('article/<int:pk>/edit/', views.ArticleUpdateView.as_view(), name='article-update')
 ]

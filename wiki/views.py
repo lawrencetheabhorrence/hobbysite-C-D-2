ddcadbadb32200dbecb7347756791aedf58ea86e
ennybor11 <ennybor11@gmail.com>
2025-04-30 14:40:25 +0800

    Edited template_name_suffix

--- wiki/views.py
+++ wiki/views.py
@@ -5,4 +5,4 @@ from .models import Article
 class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     fields = [ 'title', 'author', 'category', 'entry', 'header_image' ]
-    template_name = 'wiki/article_create.html'
+    template_name_suffix = '_create'

905a30131bb22e796fde96147d95297f8c771a8e
Goodymind <aabuke9@gmail.com>
2025-05-02 23:43:16 +0800

    category dropdown in forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,4 +1,4 @@
-from .models import Comment, Article
+from .models import Comment, Article, ArticleCategory
 from django import forms
 
 class CommentForm(forms.ModelForm):
@@ -6,7 +6,20 @@ class CommentForm(forms.ModelForm):
         model = Comment
         fields = ('entry')
 
-class ArticleForm():
+class ArticleForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        queryset=ArticleCategory.objects.all(),
+        empty_label="Select a category",
+    )
+    class Meta:
+        model = Article
+        fields = ('title', 'category', 'entry', 'header_image')
+
+class ArticleUpdateForm(forms.ModelForm):
+    category = forms.ModelChoiceField(
+        queryset=ArticleCategory.objects.all(),
+        empty_label="Select a category",
+    )
     class Meta:
         model = Article
         fields = ('title', 'category', 'entry', 'header_image')

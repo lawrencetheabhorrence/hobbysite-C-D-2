bfb0a463819f7daa1b4fd50d522ad9be3d0c81b9
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-05-04 01:09:18 +0800

    None-owners can no longer force their way in other products via url

--- merchstore/views.py
+++ merchstore/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404, reverse
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect, reverse
 from django.views import View
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView, SingleObjectMixin
@@ -126,6 +126,15 @@ class ProductCreateView(ProductMakerView, CreateView):
 
 class ProductUpdateView(ProductMakerView, UpdateView):
     template_name = "merchstore/product_update.html"
+    context_object_name = "product"
+
+    def get(self, request, *args, **kwargs):
+        self.object = Product
+        context = super().get_context_data(**kwargs)
+        affected_product = get_object_or_404(Product, pk=context['pk'])
+        if request.user.profile != affected_product.owner:
+            return redirect("merchstore:product_list")
+        return super().post(request, *args, **kwargs)
 
 
 """

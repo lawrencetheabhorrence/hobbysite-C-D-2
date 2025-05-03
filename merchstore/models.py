f8fda54e9b469d35a7018b399a98b43bb53b5df0
PeteJustinDagaraga <pete.dagaraga@student.ateneo.edu>
2025-05-03 22:32:45 +0800

    Using user.profile.products.exists and black .

--- merchstore/models.py
+++ merchstore/models.py
@@ -29,7 +29,11 @@ class Product(models.Model):
         ProductType, null=True, on_delete=models.SET_NULL, related_name="products"
     )
     owner = models.ForeignKey(
-        Profile, null=False, on_delete=models.CASCADE, related_name="products", default=""
+        Profile,
+        null=False,
+        on_delete=models.CASCADE,
+        related_name="products",
+        default="",
     )
     description = models.TextField(default="A buyable item of this merch store.")
     price = models.DecimalField(max_digits=24, decimal_places=2)
@@ -90,4 +94,4 @@ class Transaction(models.Model):
             + " currently "
             + self.status
         )
-        #Ex: Dino's 5 Chicken Nuggets currently Delivered
+        # Ex: Dino's 5 Chicken Nuggets currently Delivered

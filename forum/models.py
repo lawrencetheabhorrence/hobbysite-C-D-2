8a816db449e9e44c3713fd28fd9ebd9edfa04d54
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-05-04 13:18:17 +0800

    adjusted post detail to fit figma

--- forum/models.py
+++ forum/models.py
@@ -35,7 +35,9 @@ class Thread(models.Model):
 
 class Comment(models.Model):
     author = models.ForeignKey(Profile, null=True, on_delete=models.SET_NULL)
-    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    thread = models.ForeignKey(
+        Thread, on_delete=models.CASCADE, related_name="comments"
+    )
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)

0eaf7efe221bc1298a6cf3f0f396eed613586d17
Goodymind <aabuke9@gmail.com>
2025-05-05 18:31:21 +0800

    test cases setup 1

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Article, ArticleCategory
+from django.contrib.auth.models import User
+from user_management.models import Profile
 
-# Create your tests here.
+
+class ArticleModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="test")
+        self.profile = Profile.objects.create(user=self.user, name="name")
+        self.cat_news = ArticleCategory.objects.create(name="News", description ="")
+        self.cat_tech = ArticleCategory.objects.create(name="Tech", description ="")
+
+    def test_article_written_by_profile(self):
+        article = Article.objects.create(
+            title="Title",
+            author=self.profile,
+            article_category=self.cat_news,
+            entry="Entry"
+        )
+
+        self.assertEqual(article.title, "Title")
+        self.assertEqual(article.author, "name")
+        self.assertEqual(article.category, self.cat_news)
+        self.assertEqual(article.author, Article.objects.get(author="name").author)

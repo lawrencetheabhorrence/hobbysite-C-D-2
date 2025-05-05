80f80f9e499239e30a507f929180341b104a72eb
Goodymind <aabuke9@gmail.com>
2025-05-05 17:14:14 +0800

    views suggestions

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView
-from .models import Article
-from .forms import CommentForm, ArticleForm, ArticleUpdateForm
-
+from .models import Article, ArticleCategory
 
 class ArticleListView(ListView):
-    model = Article
+    model = ArticleCategory
     template_name = "blog/article_list.html"
-    context_object_name = "blogs"
+    context_object_name = "categories"
 
 
 class ArticleDetailView(DetailView):
@@ -17,27 +15,13 @@ class ArticleDetailView(DetailView):
     template_name = "blog/article_detail.html"
     context_object_name = "article"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["comment_form"] = CommentForm()
-        return context
-
-
 class ArticleCreateView(CreateView):
     model = Article
-    template_name = "blog/article_create_view.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["article_form"] = ArticleForm()
-        return context
-
+    template_name = "blog/article_create.html"
+    template_name_suffix = "_create"
+    fields = ["title", "category", "entry", "header_image"]
 
 class ArticleUpdateView(UpdateView):
     model = Article
     template_name = "blog/article_update_view.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["article_form"] = ArticleUpdateForm()
-        return context
+    template_name_suffix = "_update"

248cffb17f41c2c05be8d923c10ebb88cdb2d83d
Goodymind <aabuke9@gmail.com>
2025-05-05 17:37:37 +0800

    added as view to class views

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path("articles/", views.ArticleListView, name="article_list"),
-    path("article/<int:id>", views.ArticleDetailView, name="article_detail"),
-    path("article/<int:id>/edit", views.ArticleUpdateView, name="article_update"),
-    path("article/add", views.ArticleCreateView, name="article_create")
+    path("articles/", views.ArticleListView.as_view(), name="article_list"),
+    path("article/<int:id>", views.ArticleDetailView.as_view(), name="article_detail"),
+    path("article/<int:id>/edit", views.ArticleUpdateView.as_view(), name="article_update"),
+    path("article/add", views.ArticleCreateView.as_view(), name="article_create")
 ]

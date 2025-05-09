a611ae316e6adf13b41b1a82e2724882870a68b2
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-05-09 19:36:26 +0800

    ran black

--- blog/tests.py
+++ blog/tests.py
@@ -8,15 +8,15 @@ class ArticleModelTest(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="test", password="test")
         self.profile = Profile.objects.create(user=self.user, name="name")
-        self.cat_news = ArticleCategory.objects.create(name="News", description ="")
-        self.cat_tech = ArticleCategory.objects.create(name="Tech", description ="")
+        self.cat_news = ArticleCategory.objects.create(name="News", description="")
+        self.cat_tech = ArticleCategory.objects.create(name="Tech", description="")
 
     def test_article_written_by_profile(self):
         article = Article.objects.create(
             title="Title",
             author=self.profile,
             article_category=self.cat_news,
-            entry="Entry"
+            entry="Entry",
         )
 
         self.assertEqual(article.title, "Title")
--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,10 @@ app_name = "blog"
 urlpatterns = [
     path("articles/", views.ArticleListView.as_view(), name="article_list"),
     path("article/<int:id>", views.ArticleDetailView.as_view(), name="article_detail"),
-    path("article/<int:id>/edit", views.ArticleUpdateView.as_view(), name="article_update"),
-    path("article/add", views.ArticleCreateView.as_view(), name="article_create")
+    path(
+        "article/<int:id>/edit",
+        views.ArticleUpdateView.as_view(),
+        name="article_update",
+    ),
+    path("article/add", views.ArticleCreateView.as_view(), name="article_create"),
 ]

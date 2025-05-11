5ac5181021b3dd4e92cc79c9fa33fabb82998b4b
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-05-11 17:00:14 +0800

    admin for comments

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ArticleCategory, Article
+from .models import ArticleCategory, Article, Comment
 
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class ArticleAdmin(admin.ModelAdmin):
 # Register models
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory)
+admin.site.register(Comment)

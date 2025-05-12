172582ed20c8601837d0beac0ed06b005674fb87
Julyanna Francesca C. Huang <julyannahuang@gmail.com>
2025-05-12 15:22:29 +0800

    env

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -11,9 +11,15 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+DEBUG = env("DEBUG")
+SECRET_KEY = env("SECRET_KEY")
 
 
 # Quick-start development settings - unsuitable for production
